95318b04fde2586b2766437a84074c6cdf78634d
Vishal Rastogi <maganuk@hotmail.com>
2023-08-20 03:58:00 +0530

    Update the Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Session } from 'next-auth'
+import Header from '@/components/header'
+import { SessionProvider } from '@/components/session-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,12 +14,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  session,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode,
+  session: Session;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SessionProvider session={session}>
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </SessionProvider>
     </html>
   )
 }
